51aec2aac5a28fef381f31acf64fce4f53502aad
Vein <trojan_ann@aliyun.com>
2020-02-03 21:54:11 +0800

    修改标题，cdn路径

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   publicPath:
     process.env.NODE_ENV === 'production'
-      ? `https://cdn.jsdelivr.net/npm/maptools-vue@0.0.1/assets/`
+      ? `https://cdn.jsdelivr.net/npm/maptools-vue@0.0.2/docs/`
       : '/',
   outputDir: './docs/',
   assetsDir: './assets/',
   devServer: {
-    open: true
-  }
+    open: true,
+  },
 }
